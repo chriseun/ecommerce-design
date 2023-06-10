5b14bd38334a945d7ef86898ef9c5b152122b237
Chris Eun <eunchristopher@gmail.com>
2023-06-10 01:24:44 -0700

    Added Register Page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+
+`
+
+const Wrapper = styled.div`
+
+`
+
+const Title = styled.h1`
+
+`
+
+const Register = () => {
+  return (
+      <Container>
+        <Wrapper>
+          <Title>CREATE AN ACCOUNT</Title>
+        </Wrapper>
+      </Container>
+  );
+}
+
+export default Register;
